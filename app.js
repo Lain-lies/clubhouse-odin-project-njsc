35371d11a24fc847613e733cb2becd0065d90231
Kenjie Tarasona-wsl <148972543+voidKing00@users.noreply.github.com>
2026-03-21 00:10:42 +0800

    Implement logout

--- app.js
+++ app.js
@@ -37,6 +37,16 @@ app.use(passport.session());
 app.use("/", indexRouter);
 app.use("/signup", signupRouter);
 app.use("/login", loginRouter);
+app.post("/logout", (req, res) => {
+	req.logout((error) => {
+		if (error)
+			throw new Error("idk dawg something went wrong sorry bout that homeboi");
+		req.session.destroy(() => {
+			res.clearCookie("connect.sid");
+			res.redirect("/");
+		});
+	});
+});
 
 app.listen(process.env.APP_PORT, (err) => {
 	if (err) throw err;
